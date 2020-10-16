5fb958029d308bd27b561c281e1caeb1490322ab
Decston <alissonpatriciodefreitas@gmail.com>
2020-10-16 16:44:59 -0300

    Solucinado o erro de ausencia de to no componente Link.

--- src/home/index.js
+++ src/home/index.js
@@ -22,6 +22,7 @@ const Home = () => (
                 altImg={null} 
                 srcImg={null}
                 width={"auto"}
+                link='#'
             />
         </div>
         <div className="new-product-container">
@@ -31,6 +32,7 @@ const Home = () => (
                 altImg={null} 
                 srcImg={null}
                 width={"auto"}
+                link='#'
             />
         </div>
         <div className="new-product-container">
@@ -40,6 +42,7 @@ const Home = () => (
                 altImg={null} 
                 srcImg={null}
                 width={"auto"}
+                link='#'
             />
         </div>
     </div>
